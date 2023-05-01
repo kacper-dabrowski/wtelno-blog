9562e27ba5e28c4e5ba198827b28e84dc4befe18
kacper-dabrowski <kdtw301@gmail.com>
2023-05-01 14:13:59 +0200

    feat(pages): add styling for text

--- src/posts/post.tsx
+++ src/posts/post.tsx
@@ -1,17 +1,36 @@
 import { PostModel } from "@/posts/service/types";
 import { ReactComponent } from "@/shared/types/component";
 import ReactMarkdown from "react-markdown";
-
+import { Card } from "./card";
+import styles from "./post.module.scss";
+import {
+  MainHeading,
+  OrderedList,
+  ParagraphText,
+  SecondaryHeading,
+  getRendererFor,
+} from "./text/text";
+import rehypeRaw from "rehype-raw";
 interface PostProps {
   post: PostModel;
 }
 
 export const Post: ReactComponent<PostProps> = ({ post }) => {
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.createdAt.toString()}</p>
-      <ReactMarkdown>{post.content}</ReactMarkdown>
-    </div>
+    <Card additionalClasses={styles.postCard}>
+      <MainHeading>{post.title}</MainHeading>
+      {/* <p>{post.createdAt.toString()}</p> */}
+      <ReactMarkdown
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          p: getRendererFor(ParagraphText),
+          h1: getRendererFor(MainHeading),
+          h2: getRendererFor(SecondaryHeading),
+          ol: getRendererFor(OrderedList),
+        }}
+      >
+        {post.content}
+      </ReactMarkdown>
+    </Card>
   );
 };
