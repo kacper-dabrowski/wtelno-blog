a83a7729575b7cf88f84a8ee896e29903e3753eb
kacper-dabrowski <kdtw301@gmail.com>
2023-05-12 15:28:48 +0200

    feat(loading): add skeleton loading

---
+++ src/albums/loaders/singleAlbum.tsx
@@ -0,0 +1,21 @@
+import { Card } from "../../posts/card";
+import { SecondaryHeading } from "../../posts/text/text";
+import styles from "./singleAlbum.module.scss";
+import { AlbumSkeleton } from "./skeleton";
+
+const placeholderArray = new Array(9).fill(null);
+
+export const SingleAlbumLoader = () => {
+  return (
+    <Card additionalClasses={styles.card}>
+      <SecondaryHeading additionalClasses={styles.titlePlaceholder}>
+        placeholder
+      </SecondaryHeading>
+      <div className={styles.container}>
+        {placeholderArray.map((_, index) => (
+          <AlbumSkeleton key={index} />
+        ))}
+      </div>
+    </Card>
+  );
+};
