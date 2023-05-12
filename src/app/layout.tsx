a898e632f932c0da3d17ce19e429ba1348950d2c
kacper-dabrowski <kdtw301@gmail.com>
2023-05-12 16:56:39 +0200

    fix(fonts): improve font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import "./globals.scss";
 import { MainNavbar } from "@/navigation/navbar/main/mainNavbar";
+import { Montserrat } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Wtelno - Oficjalna strona wsi Wtelno",
@@ -8,13 +9,19 @@ export const metadata: Metadata = {
     "Oficjalna strona wsi Wtelno oraz informacje i aktualności z nią związane",
 };
 
+const font = Montserrat({
+  subsets: ["latin"],
+  display: "block",
+  weight: ["300", "400", "700"],
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={font.className}>
       <body>
         <MainNavbar />
         {children}
