1cd729155de51d5b6f56272418bd1284eb8e37df
kacper-dabrowski <kdtw301@gmail.com>
2023-05-15 15:07:21 +0200

    feat(bank-account-details): add styles for bank account details

--- src/posts/church/bankAccountDetails.tsx
+++ src/posts/church/bankAccountDetails.tsx
@@ -1,5 +1,7 @@
 import { ReactComponent } from "../../shared/types/component";
 import { BankAccountDetails } from "./dto";
+import styles from "./additionalContent.module.scss";
+import { ParagraphText, ThirdLevelHeading } from "../renderers/text";
 
 interface BankAccountDetailsProps extends BankAccountDetails {}
 
@@ -7,10 +9,20 @@ export const BankAccountDetailsList: ReactComponent<
   BankAccountDetailsProps
 > = ({ name, title, accountNumber }) => {
   return (
-    <div>
-      <div>{name}</div>
-      <div>{title}</div>
-      <div>{accountNumber}</div>
-    </div>
+    <section className={styles.bankAccountDetails}>
+      <ThirdLevelHeading>Dane parafialnego konta bankowego</ThirdLevelHeading>
+      <div>
+        <p className={styles.label}>Nazwa odbiorcy:</p>
+        <ParagraphText>{name}</ParagraphText>
+      </div>
+      <div>
+        <p className={styles.label}>Tytułem:</p>
+        <ParagraphText>{title}</ParagraphText>
+      </div>
+      <div>
+        <p className={styles.label}>Nr konta bankowego na cele remontowe:</p>
+        <ParagraphText>{accountNumber}</ParagraphText>
+      </div>
+    </section>
   );
 };
